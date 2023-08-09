f86088d4c56c52475a2cdbeae74a636406a15db1
Fathan Akbar A <fathan.aa.01@gmail.com>
2023-08-09 22:42:19 +0700

    improve

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,22 @@
 #include <ArduinoJson.h>
 #include <WebSocketsClient.h>
 #include <SocketIOclient.h>
+// #include <time.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-#define ssid "PDP Atas"
-#define pw "kumahaaa"
+const int PIN_AOUT = 33;
+const int PIN_OneWire = 32;
+#define ssid "samsunga8"
+#define pw "78empatx"
 
+OneWire oneWire(PIN_OneWire);
+DallasTemperature sensors(&oneWire);
 WiFiMulti WiFiMulti;
 SocketIOclient socketIO;
+WebSocketsClient wsc;
+
+int prevNow = 0;
 
 void event(
   socketIOmessageType_t type,
@@ -21,16 +31,22 @@ void event(
   {
   case sIOtype_DISCONNECT:
     Serial.println("[DISCONNECTED] ");
+    // Serial.printf("%s\n",payload);
     break;
   case sIOtype_CONNECT:
     Serial.print("[CONNECTED] ");
-    Serial.printf("%s\n",payload);
+    Serial.printf("%s %d\n",payload,length);
+    // initial http req upgrade to switcing protocol
     socketIO.send(sIOtype_CONNECT, "/");
     break;
   case sIOtype_ERROR:
     Serial.print("[ERR] ");
-    Serial.println(length);
+    Serial.printf("%s \n",payload);
     break;
+  case sIOtype_EVENT:
+    // Serial.print("[EVENT] ");
+    // Serial.printf("%s \n",payload);
+  break;
 
   default:
     break;
@@ -39,43 +55,51 @@ void event(
 
 void setup(){
   Serial.begin(115200);
+  // Serial.setDebugOutput(true);
   WiFiMulti.addAP(ssid, pw);
-
+  sensors.begin();
+  
   while(WiFiMulti.run() != WL_CONNECTED){}
 
-  String ip = WiFi.localIP().toString();
   Serial.print("WiFi Connected ");
-  Serial.println(ip);
+  Serial.println(WiFi.localIP().toString());
 
   socketIO.begin(
-    "10.133.226.120", 
+    "192.168.43.201", 
     8880, 
-    "/socket.io/?EIO=4&?transport=websocket",
-    "websocket");
+    "/socket.io/?EIO=4"
+    );
     
   socketIO.onEvent(event);
+
 }
 
 volatile unsigned long msgTs = 0;
+
 void loop(){
-  socketIO.loop();
 
   uint64_t now = millis();
 
-  if(now - msgTs > 2000){
-    msgTs = now;
+  sensors.setWaitForConversion(true);  // makes it async
+  sensors.requestTemperatures();
 
-    DynamicJsonDocument doc(1024);
-    JsonArray array = doc.to<JsonArray>();
+  socketIO.loop();
 
-    array.add("hey");
-    JsonObject param1 = array.createNestedObject();
-    param1["now"] = (uint32_t) now;
+  DynamicJsonDocument doc(1024);
+  JsonArray payload = doc.to<JsonArray>();
 
-    String output;
-    serializeJson(doc,output);
+  payload.add("ev_sn1");
+  JsonObject data = payload.createNestedObject();
+  data["suhu"] = (uint32_t) sensors.getTempCByIndex(0);
+  data["kelembaban"] = (uint32_t) analogRead(PIN_AOUT);
+  data["rssi"] = WiFi.RSSI();
+  data["latency"] = (uint32_t) millis() - now;
+  // data["latency"] = WiFi.ping("10.133.226.68");
+  
+  String output;
+  serializeJson(doc,output);
 
-    socketIO.sendEVENT(output);
+  socketIO.sendEVENT(output);
 
-  }
+  delay(1);
 }
